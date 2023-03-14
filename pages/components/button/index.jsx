8fa90d34b55c50956194e4200e6a81f850672567
Praz <t.prazwal@gmail.com>
2023-03-14 12:20:54 -0600

    added button component

---
+++ pages/components/button/index.jsx
@@ -0,0 +1,30 @@
+// Custom button
+
+import * as React from 'react';
+
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+const Button = ({customClick, title}) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={customClick}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#f05555',
+    color: '#ffffff',
+    padding: 10,
+    marginTop: 16,
+    marginLeft: 35,
+    marginRight: 35,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
+
+export default Button;
